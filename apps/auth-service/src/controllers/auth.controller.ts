d898e2cf8e0882fc13de0c7959b5052af29ea97f
1114nikhil <nikhiltk1114@gmail.com>
2025-04-30 16:56:42 +0530

    verifyUser api

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -4,9 +4,11 @@ import {
   sendOtp,
   trackOtpRequests,
   validateRegisterData,
+  verifyOtp,
 } from "../utils/auth.helper";
 import prisma from "@packages/libs/prisma";
 import { ValidationError } from "@packages/error-handler";
+import bcrypt from "bcryptjs";
 
 //Register a New User
 export const userRegistration = async (
@@ -17,19 +19,50 @@ export const userRegistration = async (
   try {
     validateRegisterData(req.body, "user");
     const { name, email } = req.body;
-    const existingUser = await prisma.users.findUnique({ where: {email}});
+    const existingUser = await prisma.users.findUnique({ where: { email } });
     if (existingUser) {
       return next(new ValidationError("User already exists with this email!"));
     }
 
     await checkOtpRestriction(email, next);
     await trackOtpRequests(email, next);
-    await sendOtp( name,email, "user-activation-mail");
+    await sendOtp(name, email, "user-activation-mail");
 
     res.status(200).json({
       message: "OTP send to mail. Please verify you account!",
     });
   } catch (error) {
     return next(error);
+  }
+};
+
+//verify user with otp
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password, otp, name } = req.body;
+    if (!email || !password || !otp || !name)
+      return next(new ValidationError("All fields are required!"));
+
+    const existingUser = await prisma.users.findUnique({ where: { email } });
+
+     
+
+    await verifyOtp(email, otp, next);
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await prisma.users.create({
+      data: { name, email, password: hashPassword },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User Register successfully!",
+    });
+  } catch (error) {
+    next(error);
   }
 };
