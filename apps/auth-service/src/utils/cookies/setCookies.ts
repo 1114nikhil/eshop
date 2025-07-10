4f6068625c50027a74ce12ba3513804479e992e3
1114nikhil <nikhiltk1114@gmail.com>
2025-07-10 05:36:48 +0530

    swagger changes

--- apps/auth-service/src/utils/cookies/setCookies.ts
+++ apps/auth-service/src/utils/cookies/setCookies.ts
@@ -4,7 +4,7 @@ export const setCookies=(res:Response,name:string,value:string)=>{
     res.cookie(name,value,{
         httpOnly:true,
         secure:true,
-        // sameSite:"none",
+        sameSite:"none",
         maxAge:7*24*60*60*1000,  // 7days
     }
     )
